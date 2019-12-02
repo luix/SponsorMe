aa2e442685aebae85fbbaec49f16538f94c0d8ba
LuiX <916632+luix@users.noreply.github.com>
2019-12-01 20:58:03 -0800

    Add a storage/Settings to the shared common module

--- SponsorMe/common/src/commonMain/kotlin/com/xinay/sponsorme/storage/Settings.kt
+++ SponsorMe/common/src/commonMain/kotlin/com/xinay/sponsorme/storage/Settings.kt
@@ -1,4 +1,8 @@
 package com.xinay.sponsorme.storage
 
 interface Settings {
+    fun putBoolean(key: String, value: Boolean)
+    fun getBoolean(key: String, defaultValue: Boolean): Boolean
+    fun putString(key: String, value: String)
+    fun getString(key: String, defaultValue: String = ""): String
 }
