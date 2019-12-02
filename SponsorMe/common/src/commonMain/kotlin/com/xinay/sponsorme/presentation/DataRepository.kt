e978e484bbfb54b8a04eadcc0aa54b309c9df506
LuiX <916632+luix@users.noreply.github.com>
2019-12-01 20:55:23 -0800

    Add a DataRepository in the shared common module

--- SponsorMe/common/src/commonMain/kotlin/com/xinay/sponsorme/presentation/DataRepository.kt
+++ SponsorMe/common/src/commonMain/kotlin/com/xinay/sponsorme/presentation/DataRepository.kt
@@ -1,4 +1,10 @@
 package com.xinay.sponsorme.presentation
 
 interface DataRepository {
+    var onRefreshListeners: List<() -> Unit>
+    var privacyPolicyAccepted: Boolean
+    var userId: String?
+
+    suspend fun update()
+    fun acceptPrivacyPolicy()
 }
