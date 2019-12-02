a7ede2c522be2f537f7d02a670d07740e3183480
LuiX <916632+luix@users.noreply.github.com>
2019-12-01 20:52:58 -0800

    Add ServiceApi.kt into commonMain module

--- SponsorMe/common/src/commonMain/kotlin/com/xinay/sponsorme/api/ServiceApi.kt
+++ SponsorMe/common/src/commonMain/kotlin/com/xinay/sponsorme/api/ServiceApi.kt
@@ -1,4 +1,44 @@
 package com.xinay.sponsorme.api
 
-interface ServiceApi {
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.header
+import io.ktor.client.request.request
+import io.ktor.client.response.HttpResponse
+import io.ktor.http.*
+import kotlinx.io.core.use
+import kotlinx.serialization.json.Json
+
+class ServiceApi(private val endPoint: String) {
+    private val client = HttpClient {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(Json.nonstrict).apply {
+                //setMapper(AllData::class, AllData.serializer())
+            }
+        }
+    }
+
+    suspend fun createUser(userId: String): Boolean = client.request<HttpResponse> {
+        apiUrl("users")
+        method = HttpMethod.Post
+        body = userId
+    }.use {
+        it.status.isSuccess()
+    }
+
+    private fun HttpRequestBuilder.json() {
+        contentType(ContentType.Application.Json)
+    }
+
+    private fun HttpRequestBuilder.apiUrl(path: String, userId: String? = null) {
+        if (userId != null) {
+            header(HttpHeaders.Authorization, "Bearer $userId")
+        }
+        header(HttpHeaders.CacheControl, "no-cache")
+        url {
+            takeFrom(endPoint)
+            encodedPath = path
+        }
+    }
 }
